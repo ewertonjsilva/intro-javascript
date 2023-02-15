85ab5c4d7b3aebcfc462879de756581fe3d01f37
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-02-15 17:30:01 -0300

    variáveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,1 +1,30 @@
+// o comando abaixo apresenta uma mensagem na tela
 console.log('Olá estou programando em javascript');
+
+/*
+    comentário que ocupa mais
+    de uma linha
+*/
+
+// criação de variável do tipo texto
+let nome = 'Ewerton José da Silva'; 
+// criação de variável do tipo numérico (inteiro)
+let idade = 35;
+// criação de variável do tipo numério (decimal)
+let altura = 1.75; 
+// criação de variável do tipo verdadeiro/falso
+let trabalha = true; // true = verdadeiro | false = falso
+// exemplo de variável de nome composto
+let corFavorita = 'Laranja';
+// exemplo de variável do tipo lista de valores
+let cores_favoritas = ['Laranja', 'Roxo', 'Azul'];
+// exemplo de variável que armazena um objeto
+let endereco = {
+    'logradouro'        : 'Rua', 
+    'nome_logradouro'   : 'Brasil', 
+    'número'            : '50 A', 
+    'bairro'            : 'Centro', 
+    'cidade'            : 'Tupã'
+};
+let covid;
+let dengue = null;
