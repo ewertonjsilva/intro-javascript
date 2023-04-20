f1c161da8dd4eb0169afa3b8f6a447f75b061562
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-04-19 21:11:18 -0300

    aula 05-04

---
+++ exemplos/009-atividade.js
@@ -0,0 +1,8 @@
+/*
+    Crie um programa que receba duas notas
+    calcule a média entre elas e apresente
+    o texto aprovado caso a média seja >= 7
+    e reprovado caso seja menor.
+    A notas digitadas devem estar entre 0 e 10!
+*/
+
