e691087b57e9b55ce8fb012be84cb903a075dc05
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-05-03 17:28:49 -0300

    aula 3-5-23

--- exemplos/015-atividade-rep.js
+++ exemplos/015-atividade-rep.js
@@ -4,3 +4,26 @@
   digite a senha corretamente e "Usuário Bloqueado" 
   caso o usuário erre a senha 3 vezes.
 */
+
+
+const entrada = require('prompt-sync')(); 
+
+const senha = '1357908642';
+let senhaDigitada = '';
+let erros = -1;
+
+do {
+    erros++;
+
+    if (erros >= 3) {
+      break;
+    }
+
+    senhaDigitada = entrada('Digite sua senha para entrar! ');
+} while (senha != senhaDigitada);
+
+if (erros < 3) {
+  console.log('Você acessou o sistema!');
+} else {
+  console.log('Você errou a senha 3 vezes, seu acesso foi bloqueado!');
+}
---
+++ exemplos/016-array.js
@@ -0,0 +1,24 @@
+
+// Array
+
+/*
+    let aluno1 = 'Teófilo Trajano';
+    let aluno2 = 'Raíssa Rebeca'; 
+    let aluno3 = 'Carlos Caetano'; 
+    let aluno4 = 'Helna Harris'; 
+
+    console.log('Lista de alunos:'); 
+    console.log('- ' + aluno1);
+    console.log('- ' + aluno2);
+    console.log('- ' + aluno3);
+    console.log('- ' + aluno4);
+*/
+
+let alunos = [
+    'Teófilo Trajano', 
+    'Raíssa Rebeca', 
+    'Carlos Caetano', 
+    'Helna Harris'
+];
+
+console.log(alunos);
